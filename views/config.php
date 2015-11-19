00a1806409208c9f920708a0760b52cd0f396134
Will Scott <willscott@gmail.com>
2015-11-19 11:34:18 +0800

    visual indication that full caching doesn't need offline page.

--- views/config.php
+++ views/config.php
@@ -24,6 +24,29 @@
                       <p><label for="activist_mode_2"><input type="radio" name="activist_cache_mode" id="activist_mode_2" value="2" <?php checked('2', $cache_mode); ?> /> <?php esc_html_e('Clone full blog for offline display.', 'activist'); ?></label></p>
 											<!--<p><label for="activist_mode_3"><input type="radio" name="activist_cache_mode" id="activist_mode_3" value="3" <?php checked('3', $cache_mode); ?> /> <?php esc_html_e('Clone some pages for offline display.', 'activist'); ?></label></p>-->
                       </fieldset>
+											<script type='text/javascript'>
+												window.addEventListener('load', function() {
+													var m1 = document.getElementById('activist_mode_1');
+													var m2 = document.getElementById('activist_mode_2');
+
+													var onChange = function() {
+														var ol = document.getElementById('offline_label');
+														var o1 = document.getElementById('activist_offline_warn');
+														var o2 = document.getElementById('activist_offline_post');
+														var os = document.getElementById('activist_offline_chooser');
+														if (m1.checked) {
+															ol.style.color = 'inherit';
+															os.disabled = o1.disabled = o2.disabled = false;
+														} else {
+															ol.style.color = '#888';
+															os.disabled = o1.disabled = o2.disabled = true;
+														}
+													};
+													onChange();
+													m1.addEventListener('change', onChange, true);
+													m2.addEventListener('change', onChange, true);
+												}, true);
+											</script>
       							</td>
                   </tr>
                   <tr>
@@ -38,7 +61,7 @@
       							</td>
                   </tr>
 									<tr>
-										<th align="left" scope="row"><?php esc_html_e('When Offline', 'activist');?></th>
+										<th align="left" scope="row" id='offline_label'><?php esc_html_e('When Offline', 'activist');?></th>
 										<td></td>
 										<td align="left">
 											<p>
